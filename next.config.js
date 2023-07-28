bb7bb4957a3dbaf923e92da6bb1a04e995a7ae0e
Paul Schneider <paul.schneider4@gmail.com>
2023-07-28 15:54:33 -0400

    react-native alias note

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = withExpo({
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: [
+    // NOTE: Even though `react-native` is never used in Next.js,
+    // you need to list `react-native` because `react-native-web`
+    // is aliased to `react-native`. Adding `react-native-web` will not work.
     "react-native",
     "expo",
     // Add more React Native / Expo packages here...
